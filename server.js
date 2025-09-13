5a6309c85596980d6ed7db0ae2bbfc81ca48bdf1
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-09-13 10:59:55 +0530

    install dotenv and nodemon

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./src/app");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port, ${PORT}...`);
